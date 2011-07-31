7d20bc5be40789854175004e7bd14a78ec000635
petar.perisin@gmail.com <petar.perisin@gmail.com@850c2b7b-82bf-3a68-b65d-028da4c654c5>
2011-07-31 21:44:13 +0000

    Updated FormatPaintBrush script with dimension and started to implement same thing for sch wire.

--- trunk/FormatPaintBrush/FormatPaintBrush.pas
+++ trunk/FormatPaintBrush/FormatPaintBrush.pas
@@ -1,14 +1,21 @@
 function FormatPaintBrush;
 var
    // SCH variables and objects
-   SchDoc : ISCH_Document;
+   SchDoc          : ISCH_Document;
+   Location        : TLocation;
+   SchSourcePrim   : TObject;
+   SchDestinPrim   : TObject;
+   SpatialIterator : ISch_Iterator;
 
    // PCB variables and objects
    PCBBoard      : IPCB_Board;
    SourcePrim    : IPCB_Primitive;
    DestinPrim    : IPCB_Primitive;
    BoardIterator : IPCB_BoardIterator;
+
    // Common variables
+   boolLoc       : bool;
+
 Begin
    If GetWorkspace.DM_FocusedDocument.DM_DocumentKind = 'SCH' then
    Begin
@@ -16,6 +23,93 @@ Begin
       if SchServer = nil then exit;
       SchDoc := SchServer.GetCurrentSchDocument;
       if SchDoc = nil then exit;
+
+      ResetParameters;
+      AddStringParameter('Action', 'AllOpenDocuments');
+      RunProcess('Sch:DeSelect');
+
+      Location := TLocation;
+
+      // Get Source Object
+      boolLoc := SchDoc.ChooseLocationInteractively(Location, 'Choose Source Object');
+
+      If Not boolLoc Then Exit;
+
+      SpatialIterator := SchDoc.SchIterator_Create;
+      If SpatialIterator = Nil Then Exit;
+      Try
+         SpatialIterator.AddFilter_Area(Location.X - 1, Location.Y - 1, Location.X + 1, Location.Y + 1);
+
+         SchSourcePrim := SpatialIterator.FirstSchObject;
+      Finally
+         SchDoc.SchIterator_Destroy(SpatialIterator);
+      End;
+
+      // Get First Destination Object
+      boolLoc := SchDoc.ChooseLocationInteractively(Location, 'Choose Destination Object');
+
+      If Not boolLoc Then Exit;
+
+      SpatialIterator := SchDoc.SchIterator_Create;
+      If SpatialIterator = Nil Then Exit;
+      Try
+         SpatialIterator.AddFilter_ObjectSet(MkSet(SchSourcePrim.ObjectId));
+         SpatialIterator.AddFilter_Area(Location.X - 1, Location.Y - 1, Location.X + 1, Location.Y + 1);
+
+         SchDestinPrim := SpatialIterator.FirstSchObject;
+      Finally
+         SchDoc.SchIterator_Destroy(SpatialIterator);
+      End;
+
+      While (SchSourcePrim <> nil) and (SchDestinPrim <> nil) do
+      begin
+         SchServer.ProcessControl.PreProcess(SchDoc, '');
+
+         SchServer.RobotManager.SendMessage(SchDestinPrim.I_ObjectAddress, c_BroadCast, SCHM_BeginModify, c_NoEventData);
+
+         // This is where modification goes.
+         if SchSourcePrim.ObjectId = eWire then
+         begin
+            SchDestinPrim.LineStyle := SchSourcePrim.LineStyle;
+            SchDestinPrim.LineWidth := SchSourcePrim.LineWidth;
+            SchDestinPrim.Color     := SchSourcePrim.Color;     
+
+         //---------------------------------------------------------------------
+         //
+         //----------------      NEW SCH OBJECTS HERE      ---------------------
+         //
+         //---------------------------------------------------------------------
+         //
+         // if you want to add some new SCH objects, uncomment this
+     (*
+         end
+         else if (SchSourcePrim.ObjectId = <ObjectId-here>) then
+         begin
+            // This is where object properties are being copies
+     *)
+
+         end;
+         SchServer.RobotManager.SendMessage(SchDestinPrim.I_ObjectAddress, c_BroadCast, SCHM_EndModify  , c_NoEventData);
+
+         SchServer.ProcessControl.PostProcess(SchDoc, '');
+
+         // Get Next Destination Object
+         boolLoc := SchDoc.ChooseLocationInteractively(Location, 'Choose Destination Object');
+
+         If Not boolLoc Then Exit;
+
+         SpatialIterator := SchDoc.SchIterator_Create;
+         If SpatialIterator = Nil Then Exit;
+         Try
+            SpatialIterator.AddFilter_ObjectSet(MkSet(SchSourcePrim.ObjectId));
+            SpatialIterator.AddFilter_Area(Location.X - 1, Location.Y - 1, Location.X + 1, Location.Y + 1);
+
+            SchDestinPrim := SpatialIterator.FirstSchObject;
+         Finally
+            SchDoc.SchIterator_Destroy(SpatialIterator);
+         End;
+      end;
+
    End
    Else If GetWorkspace.DM_FocusedDocument.DM_DocumentKind = 'PCB' then
    Begin
@@ -63,7 +157,6 @@ Begin
                  DestinPrim.ExtensionPickGap   := SourcePrim.ExtensionPickGap;
                  DestinPrim.Style              := SourcePrim.Style;
                  DestinPrim.UseTTFonts         := SourcePrim.UseTTFonts;
-                 DestinPrim.Layer              := SourcePrim.Layer;
                  DestinPrim.Bold               := SourcePrim.Bold;
                  DestinPrim.Italic             := SourcePrim.Italic;
                  DestinPrim.FontName           := SourcePrim.FontName;
@@ -76,12 +169,51 @@ Begin
                  DestinPrim.TextX              := DestinPrim.TextX + MilsToCoord(0.01);
                  DestinPrim.SetState_XSizeYSize;
                  DestinPrim.GraphicallyInvalidate;
-                 (*
+                 DestinPrim.TextX              := DestinPrim.TextX - MilsToCoord(0.01);
+                 DestinPrim.SetState_XSizeYSize;
+                 DestinPrim.GraphicallyInvalidate;
+
              End
              else if (SourcePrim.ObjectId = eCoordinateObject) Then
              begin
-                // I need to put something here
-                   *)
+                // this is where we put coordinate formatting
+                 DestinPrim.Size               := SourcePrim.Size;
+                 DestinPrim.LineWidth          := SourcePrim.LineWidth;
+                 DestinPrim.TextHeight         := SourcePrim.TextHeight;
+                 DestinPrim.TextWidth          := SourcePrim.TextWidth;
+                 DestinPrim.TextFont           := SourcePrim.TextFont;
+                 DestinPrim.Style              := SourcePrim.Style;
+                 DestinPrim.UseTTFonts         := SourcePrim.UseTTFonts;
+                 DestinPrim.Bold               := SourcePrim.Bold;
+                 DestinPrim.Italic             := SourcePrim.Italic;
+                 DestinPrim.FontName           := SourcePrim.FontName;
+
+                 // !!! Workaround for now - needed to fake Dimension has changed semantics. This
+                 // is necesary because we don't currenly have access to the Dimension method that
+                 // force a dimension update. Without this the call to DestinPrim.SetState_XSizeYSize
+                 // is not doing anything
+                 DestinPrim.X     := DestinPrim.X + MilsToCoord(0.01);
+                 DestinPrim.SetState_XSizeYSize;
+                 DestinPrim.GraphicallyInvalidate;
+                 DestinPrim.X     := DestinPrim.X - MilsToCoord(0.01);
+                 DestinPrim.SetState_XSizeYSize;
+                 DestinPrim.GraphicallyInvalidate;
+
+             //-----------------------------------------------------------------
+             //
+             //----------      NEW PCB OBJECTS HERE      -----------------------
+             //
+             //-----------------------------------------------------------------
+             //
+             // if you want to add some new PCB objects, uncomment this
+          (*
+             end
+             else if (SourcePrim.ObjectId = <ObjectId-here>) then
+             begin
+               // This is where object properties are being copies
+          *) 
+
+
              end;
              // Always use IPCB_Primitive.EndModify / CancelMdify instead of PCBServer.SendMessageToRobots because is deprecated
              DestinPrim.EndModify;
