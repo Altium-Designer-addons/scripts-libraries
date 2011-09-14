ddad75d1088bb5b6d956a645b45225b180c0d50c
petar.perisin <petar.perisin@gmail.com>
2011-09-14 11:34:00 +0000

    Fixed the way FlipComponents work - not it uses smart way of flipping completely.

--- FlipComponents/FlipComponents.pas
+++ FlipComponents/FlipComponents.pas
@@ -6,7 +6,9 @@
 {             components, and it flips them.                                   }
 {                                                                              }
 {           Script uses smart way to figure out weather component is vertical  }
-{           or horizontal, and it flips according to this info.                }
+{           or horizontal, and it flips according to this info. This means     }
+{           that for simple components like resistors pads will not change     }
+{           position, and even if flipped they will be flipped in a smart way. }
 {                                                                              }
 {                                                                              }
 { Created by:    Petar Perisin                                                 }
@@ -15,20 +17,26 @@
 {..............................................................................}
 procedure Calculate(Component : IPCB_Component);
 var
-   CompIterator  : IPCB_GroupIterator;
-   Primitive     : IPCB_Primitive;
-   Pad           : IPCB_Pad2;
-   OldRotation   : Float;
-   DictionaryX   : TStringList;
-   DictionaryY   : TStringList;
-   Line          : String;
-   Location      : String;
-   Number        : String;
-   i             : Integer;
-   Indeks        : Integer;
-   Num           : Integer;
-   MaxX          : Integer;
-   MaxY          : Integer;
+   CompIterator   : IPCB_GroupIterator;
+   Primitive      : IPCB_Primitive;
+   Pad            : IPCB_Pad2;
+   Tekst          : IPCB_Text;
+   OldRotation    : Float;
+   DictionaryX    : TStringList;
+   DictionaryY    : TStringList;
+   Line           : String;
+   Location       : String;
+   Number         : String;
+   i              : Integer;
+   Indeks         : Integer;
+   Num            : Integer;
+   MaxX           : Integer;
+   MaxY           : Integer;
+   Rectangle      : TCoordRect;
+   BoundRect      : TCoordRect;
+   X1, Y1, X2, Y2 : Integer;
+   X, Y           : Integer;
+   Temp           : Integer;
 begin
 
    OldRotation := Component.Rotation;
@@ -50,7 +58,6 @@ begin
    MaxY := 1;
 
    Pad := CompIterator.FirstPCBObject;
-
 
    While (Pad <> Nil) Do
    Begin
@@ -89,28 +96,194 @@ begin
    Component.GroupIterator_Destroy(CompIterator);
 
 
-   Component.BeginModify;
+
    if MaxY >= MaxX then
    begin
-      // This is vertical component
-      Component.Rotation := Oldrotation + 180;
-      Component.Name.RotateBy(180);
-      Component.Name.MoveByXY(2 * (Component.x - Component.Name.XLocation), 2 * (Component.y - Component.Name.YLocation));
 
+
+      // This is Horizontal component
+      Component.Rotation := Oldrotation;
+
+      CompIterator := Component.GroupIterator_Create;
+      CompIterator.AddFilter_ObjectSet(MkSet(ePadObject));
+
+      Pad := CompIterator.FirstPCBObject;
+
+      BoundRect := TCoordRect;
+
+      if Pad <> nil then
+      begin
+         BoundRect.Left   := Pad.X;
+         BoundRect.Right  := Pad.X;
+         BoundRect.Top    := Pad.Y;
+         BoundRect.Bottom := Pad.Y;
+      end;
+
+      Temp := 0;
+
+      While (Pad <> Nil) Do
+      Begin
+
+         Inc(Temp);
+
+         Rectangle := Pad.BoundingRectangle;
+
+         if Rectangle.Left   < BoundRect.Left   then BoundRect.Left   := Rectangle.Left;
+         if Rectangle.Right  > BoundRect.Right  then BoundRect.Right  := Rectangle.Right;
+         if Rectangle.Top    > BoundRect.Top    then BoundRect.Top    := Rectangle.Top;
+         if Rectangle.Bottom < BoundRect.Bottom then BoundRect.Bottom := Rectangle.Bottom;
+
+         Pad := CompIterator.NextPCBObject;
+      End;
+      Component.GroupIterator_Destroy(CompIterator);
+
+      X1 := BoundRect.Left;
+      Y1 := BoundRect.Bottom;
+
+      X := (BoundRect.Left + BoundRect.Right) / 2;
+      Y := (BoundRect.Top + BoundRect.Bottom) / 2;
+
+      // This is Horizontal component
+      Component.Rotation := Oldrotation + 180;
+
+      Component.BeginModify;
       if Component.Layer = eTopLayer then Component.Layer := eBottomLayer
       else                                Component.Layer := eTopLayer;
+      Component.EndModify;
+
+      CompIterator := Component.GroupIterator_Create;
+      CompIterator.AddFilter_ObjectSet(MkSet(ePadObject));
+
+      Pad := CompIterator.FirstPCBObject;
+
+      if Pad <> nil then
+      begin
+         BoundRect.Left   := Pad.X;
+         BoundRect.Right  := Pad.X;
+         BoundRect.Top    := Pad.Y;
+         BoundRect.Bottom := Pad.Y;
+      end;
+
+      While (Pad <> Nil) Do
+      Begin
+
+         Rectangle := Pad.BoundingRectangle;
+
+         if Rectangle.Left   < BoundRect.Left   then BoundRect.Left   := Rectangle.Left;
+         if Rectangle.Right  > BoundRect.Right  then BoundRect.Right  := Rectangle.Right;
+         if Rectangle.Top    > BoundRect.Top    then BoundRect.Top    := Rectangle.Top;
+         if Rectangle.Bottom < BoundRect.Bottom then BoundRect.Bottom := Rectangle.Bottom;
+
+         Pad := CompIterator.NextPCBObject;
+      End;
+      Component.GroupIterator_Destroy(CompIterator);
+
+      X2 := BoundRect.Left;
+      Y2 := BoundRect.Bottom;
+
+      Component.MoveByXY(X1 - X2, Y1 - Y2);
+
+      // we will modify tekst placement for this kind of rotation
+      CompIterator := Component.GroupIterator_Create;
+      CompIterator.AddFilter_ObjectSet(MkSet(eTextObject));
+
+      Tekst := CompIterator.FirstPCBObject;
+
+      While (Tekst <> Nil) Do
+      Begin
+         Tekst.RotateBy(180);
+         Tekst.MoveByXY(2 * (X - Tekst.XLocation), 2 * (Y - Tekst.YLocation));
+
+         Tekst := CompIterator.NextPCBObject;
+      End;
+      Component.GroupIterator_Destroy(CompIterator);
+
+      Component.Name.RotateBy(180);
+      Component.Name.MoveByXY(2 * (X - Component.Name.XLocation), 2 * (Y - Component.Name.YLocation));
+
+      Component.Comment.RotateBy(180);
+      Component.Comment.MoveByXY(2 * (X - Component.Comment.XLocation), 2 * (Y - Component.Comment.YLocation));
+
 
    end
    else
    begin
-      // This is horizontal component
+      // This is Vertical component
       Component.Rotation := Oldrotation;
 
+      CompIterator := Component.GroupIterator_Create;
+      CompIterator.AddFilter_ObjectSet(MkSet(ePadObject));
+
+      Pad := CompIterator.FirstPCBObject;
+
+      BoundRect := TCoordRect;
+
+      if Pad <> nil then
+      begin
+         BoundRect.Left   := Pad.X;
+         BoundRect.Right  := Pad.X;
+         BoundRect.Top    := Pad.Y;
+         BoundRect.Bottom := Pad.Y;
+      end;
+
+      Temp := 0;
+
+      While (Pad <> Nil) Do
+      Begin
+
+         Inc(Temp);
+
+         Rectangle := Pad.BoundingRectangle;
+
+         if Rectangle.Left   < BoundRect.Left   then BoundRect.Left   := Rectangle.Left;
+         if Rectangle.Right  > BoundRect.Right  then BoundRect.Right  := Rectangle.Right;
+         if Rectangle.Top    > BoundRect.Top    then BoundRect.Top    := Rectangle.Top;
+         if Rectangle.Bottom < BoundRect.Bottom then BoundRect.Bottom := Rectangle.Bottom;
+
+         Pad := CompIterator.NextPCBObject;
+      End;
+      Component.GroupIterator_Destroy(CompIterator);
+
+      X1 := BoundRect.Left;
+      Y1 := BoundRect.Bottom;
+
+      Component.BeginModify;
       if Component.Layer = eTopLayer then Component.Layer := eBottomLayer
       else                                Component.Layer := eTopLayer;
+      Component.EndModify;
 
+      CompIterator := Component.GroupIterator_Create;
+      CompIterator.AddFilter_ObjectSet(MkSet(ePadObject));
+
+      Pad := CompIterator.FirstPCBObject;
+
+      if Pad <> nil then
+      begin
+         BoundRect.Left   := Pad.X;
+         BoundRect.Right  := Pad.X;
+         BoundRect.Top    := Pad.Y;
+         BoundRect.Bottom := Pad.Y;
+      end;
+
+      While (Pad <> Nil) Do
+      Begin
+
+         Rectangle := Pad.BoundingRectangle;
+
+         if Rectangle.Left   < BoundRect.Left   then BoundRect.Left   := Rectangle.Left;
+         if Rectangle.Right  > BoundRect.Right  then BoundRect.Right  := Rectangle.Right;
+         if Rectangle.Top    > BoundRect.Top    then BoundRect.Top    := Rectangle.Top;
+         if Rectangle.Bottom < BoundRect.Bottom then BoundRect.Bottom := Rectangle.Bottom;
+
+         Pad := CompIterator.NextPCBObject;
+      End;
+      Component.GroupIterator_Destroy(CompIterator);
+
+      X2 := BoundRect.Left;
+      Y2 := BoundRect.Bottom;
+
+      Component.MoveByXY(X1 - X2, Y1 - Y2);
    end;
-   Component.EndModify;
 end;
 
 
@@ -127,10 +300,11 @@ begin
    if Board.SelectecObjectCount = 0 then
    begin
       Component := Board.GetObjectAtCursor(MkSet(eComponentObject),AllLayers, 'Choose Component');
-      repeat
+      While Component <> nil do
+      begin
          Calculate(Component);
          Component := Board.GetObjectAtCursor(MkSet(eComponentObject),AllLayers, 'Choose Component');
-      until Component = nil;
+      end;
    end
    else
    begin
