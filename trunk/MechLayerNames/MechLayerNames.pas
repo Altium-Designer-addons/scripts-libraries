7294ca4dfe3d816c5b68ce0f72b108313481853e
petar.perisin@gmail.com <petar.perisin@gmail.com@850c2b7b-82bf-3a68-b65d-028da4c654c5>
2011-09-20 21:34:48 +0000

    added CopyDesignatorsToMechLayerPair - script that copies designators to mech layer pair also added header to MechLayerNames script

--- trunk/MechLayerNames/MechLayerNames.pas
+++ trunk/MechLayerNames/MechLayerNames.pas
@@ -1,3 +1,13 @@
+{..............................................................................}
+{ Summary   This scripts can be used to copy mech layer names to a *.txt file. }
+{                                                                              }
+{           After that, layer names can be imported in another PcbDoc file     }
+{                                                                              }
+{                                                                              }
+{ Created by:    Petar Perisin                                                 }
+{..............................................................................}
+
+{..............................................................................}
 Procedure ExportMechLayerNames;
 var
    Board      : IPCB_Board;
