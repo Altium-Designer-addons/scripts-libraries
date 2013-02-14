c0904f9fb40f11ccb7ea2b8338bd391f2fa330f5
gtcee3 <gtcee3@gmail.com>
2013-02-14 14:13:52 +0000

    Added support to show/hide comments on all components as well.

--- ShowHideDesignators/ShowHideDesignators.dfm
+++ ShowHideDesignators/ShowHideDesignators.dfm
@@ -1,43 +1,60 @@
 object FormShowHideDesignators: TFormShowHideDesignators
   Left = 16
   Top = 15
-  Caption = 'Show/Hide Designators'
-  ClientHeight = 128
-  ClientWidth = 258
+  Margins.Left = 0
+  Margins.Top = 0
+  Margins.Right = 0
+  Margins.Bottom = 0
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsToolWindow
+  Caption = 'Show/Hide Designators & Comments'
+  ClientHeight = 96
+  ClientWidth = 250
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  FormStyle = fsStayOnTop
   OldCreateOrder = False
+  OnClose = FormShowHideClose
+  OnCreate = FormShowHideDesignatorsCreate
+  FormKind = fkModal
   PixelsPerInch = 96
   TextHeight = 13
-  object ButtonShow: TButton
-    Left = 16
-    Top = 16
-    Width = 75
-    Height = 25
-    Caption = 'Show All'
-    TabOrder = 0
-    OnClick = ButtonShowClick
-  end
-  object ButtonHide: TButton
-    Left = 16
-    Top = 48
-    Width = 75
-    Height = 25
-    Caption = 'Hide All'
-    TabOrder = 1
-    OnClick = ButtonHideClick
-  end
-  object ButtonClose: TButton
+  object btnClose: TButton
     Left = 160
-    Top = 88
+    Top = 56
     Width = 75
     Height = 25
     Caption = 'Close'
-    TabOrder = 2
+    TabOrder = 0
     OnClick = ButtonCloseClick
+  end
+  object chkDesignator: TCheckBox
+    Left = 16
+    Top = 24
+    Width = 97
+    Height = 17
+    Caption = 'Show Designator'
+    TabOrder = 1
+  end
+  object chkComment: TCheckBox
+    Left = 16
+    Top = 48
+    Width = 97
+    Height = 17
+    Caption = 'Show Comment'
+    TabOrder = 2
+  end
+  object btnApply: TButton
+    Left = 160
+    Top = 24
+    Width = 75
+    Height = 25
+    Caption = 'Apply'
+    TabOrder = 3
+    OnClick = ButtonApplyClick
   end
 end
--- ShowHideDesignators/ShowHideDesignators.pas
+++ ShowHideDesignators/ShowHideDesignators.pas
@@ -1,79 +1,73 @@
 {..............................................................................}
-{ Summary   This script allows user to easily show or hide Designators.        }
+{ Summary   This script allows user to easily show or hide Designators and     }
+{           Comments.                                                          }
 {                                                                              }
 { Created by:    Ran Shah  ar                                                  }
+{ Changelog:                                                                   }
+{   2013-02-14   Cyril Andreatta                                               }
+{                Added support to show/hide comments                           }
 {..............................................................................}
 
-{..............................................................................}
+{***
+ * global variables for iterating over components
+}
+var
+    ComponentIteratorHandle : IPCB_BoardIterator;
+    Component               : IPCB_Component;
+
+{***
+ * procedure to show or hide names and comments for all components
+}
+procedure ShowHide (nameon : boolean, commenton : boolean);
+begin
+    Component := ComponentIteratorHandle.FirstPCBObject;
+    While (Component <> Nil) Do
+    Begin
+        Component.NameOn := nameon;
+        Component.CommentOn := commenton;
+
+       Component := ComponentIteratorHandle.NextPCBObject;
+    end;
+
+    ResetParameters;
+    AddStringParameter('Action','Redraw');
+    RunProcess('PCB:Zoom');
+end;
+
+{***
+ * actions performed on button press
+}
+procedure TFormShowHideDesignators.ButtonApplyClick(Sender: TObject);
+begin
+   ShowHide (chkDesignator.Checked, chkComment.Checked);
+end;
 
 procedure TFormShowHideDesignators.ButtonCloseClick(Sender: TObject);
 begin
-   close;
+    close;
 end;
 
-
-
-procedure TFormShowHideDesignators.ButtonShowClick(Sender: TObject);
-Var
-    ComponentIteratorHandle : IPCB_BoardIterator;
-    Component               : IPCB_Component;
+{***
+ * Create BoardIterator when form is created. BoardIterator is active as long
+ * as the script is running.
+}
+procedure TFormShowHideDesignators.FormShowHideDesignatorsCreate(Sender: TObject);
 begin
     ComponentIteratorHandle := PCBServer.GetCurrentPCBBoard.BoardIterator_Create;
     ComponentIteratorHandle.AddFilter_ObjectSet(MkSet(eComponentObject));
     ComponentIteratorHandle.AddFilter_LayerSet(AllLayers);
     ComponentIteratorHandle.AddFilter_Method(eProcessAll);
 
+    // take first found component as reference and display it's state in checkbox
     Component := ComponentIteratorHandle.FirstPCBObject;
-    While (Component <> Nil) Do
-    Begin
-       Component.NameOn := True;
-
-       Component := ComponentIteratorHandle.NextPCBObject;
-    end;
-
-    PCBServer.GetCurrentPCBBoard.BoardIterator_Destroy(ComponentIteratorHandle);
-
-    ResetParameters;
-    AddStringParameter('Action','Redraw');
-    RunProcess('PCB:Zoom');
-
-    close;
+    chkComment.Checked := Component.CommentOn;
+    chkDesignator.Checked := Component.NameOn;
 end;
 
-
-
-procedure TFormShowHideDesignators.ButtonHideClick(Sender: TObject);
-Var
-    ComponentIteratorHandle : IPCB_BoardIterator;
-    Component               : IPCB_Component;
+{***
+ * destroy global BoardIterator on FormClose
+}
+procedure TFormShowHideDesignators.FormShowHideClose(Sender: TObject; var Action: TCloseAction);
 begin
-    ComponentIteratorHandle := PCBServer.GetCurrentPCBBoard.BoardIterator_Create;
-    ComponentIteratorHandle.AddFilter_ObjectSet(MkSet(eComponentObject));
-    ComponentIteratorHandle.AddFilter_LayerSet(AllLayers);
-    ComponentIteratorHandle.AddFilter_Method(eProcessAll);
-
-    Component := ComponentIteratorHandle.FirstPCBObject;
-    While (Component <> Nil) Do
-    Begin
-       Component.NameOn := False;
-
-       Component := ComponentIteratorHandle.NextPCBObject;
-    end;
-
     PCBServer.GetCurrentPCBBoard.BoardIterator_Destroy(ComponentIteratorHandle);
-
-    ResetParameters;
-    AddStringParameter('Action','Redraw');
-    RunProcess('PCB:Zoom');
-
-    close;
-end;
-
-
-
-Procedure Start;
-begin
-   if PCBServer.GetCurrentPCBBoard = nil then exit;
-
-   FormShowHideDesignators.ShowModal;
 end;
