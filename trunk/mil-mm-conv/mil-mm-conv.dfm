cc581bd8929595d34049c530e864a04dae8c4dbe
justin.massiot@gmail.com <justin.massiot@gmail.com@850c2b7b-82bf-3a68-b65d-028da4c654c5>
2014-07-11 08:12:58 +0000

    mil-mm-conv script improved with new features: * New unit available: inch, recognized as 'in' in the textbox input * Ability to copy the result to the clipboard automatically (user-selectable by a checkbox) * New parameters for user configuration: output format, config to copy to clipboard, color configuration * New GUI for the new features listed above

--- trunk/mil-mm-conv/mil-mm-conv.dfm
+++ trunk/mil-mm-conv/mil-mm-conv.dfm
@@ -3,8 +3,8 @@ object frmCalc: TfrmCalc
   Top = 0
   BorderStyle = bsToolWindow
   Caption = 'metric <-> imperial'
-  ClientHeight = 228
-  ClientWidth = 147
+  ClientHeight = 409
+  ClientWidth = 185
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,90 +13,210 @@ object frmCalc: TfrmCalc
   Font.Style = []
   FormStyle = fsStayOnTop
   OldCreateOrder = False
+  OnCreate = InitCreate
+  OnShow = InitShow
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 5
+    Width = 170
+    Height = 16
+    Alignment = taCenter
+    AutoSize = False
+    Caption = 'Please enter a dimension'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label2: TLabel
+    Left = 8
+    Top = 21
+    Width = 170
+    Height = 16
+    Alignment = taCenter
+    AutoSize = False
+    Caption = 'with its unit'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object lblUnit: TLabel
+    Left = 145
+    Top = 64
+    Width = 30
+    Height = 13
+    AutoSize = False
+    Caption = '    '
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object lblClipBrd: TLabel
+    Left = 4
+    Top = 356
+    Width = 176
+    Height = 16
+    Alignment = taCenter
+    AutoSize = False
+    Caption = 'Clipboard filled in!'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    Transparent = True
+    Visible = False
+  end
+  object Label3: TLabel
+    Left = 8
+    Top = 37
+    Width = 170
+    Height = 16
+    Alignment = taCenter
+    AutoSize = False
+    Caption = '('#39'in'#39', '#39'mil'#39', '#39'mm'#39' or '#39'um'#39')'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
   object grpcalc: TGroupBox
     Left = 16
-    Top = 8
-    Width = 112
-    Height = 152
-    Caption = 'metric <-> imperial'
-    TabOrder = 0
-    object lblum: TLabel
-      Left = 80
-      Top = 56
-      Width = 14
-      Height = 13
-      Caption = 'um'
-    end
-    object lblmm: TLabel
-      Left = 80
-      Top = 88
-      Width = 16
-      Height = 13
-      Caption = 'mm'
-    end
+    Top = 164
+    Width = 152
+    Height = 88
+    Caption = 'Imperial'
+    TabOrder = 3
     object lblmil: TLabel
-      Left = 80
-      Top = 115
+      Left = 120
+      Top = 29
       Width = 12
       Height = 13
       Caption = 'mil'
     end
-    object edInput: TEdit
-      Left = 8
-      Top = 16
-      Width = 64
+    object lblinch: TLabel
+      Left = 120
+      Top = 61
+      Width = 19
+      Height = 13
+      Caption = 'inch'
+    end
+    object edmil: TEdit
+      Left = 16
+      Top = 24
+      Width = 88
       Height = 21
       Alignment = taRightJustify
+      Color = clMenu
+      Ctl3D = True
+      ParentCtl3D = False
       TabOrder = 0
     end
-    object edmm: TEdit
-      Left = 8
-      Top = 80
-      Width = 64
+    object edinch: TEdit
+      Left = 16
+      Top = 56
+      Width = 88
       Height = 21
       Alignment = taRightJustify
       Color = clMenu
       TabOrder = 1
     end
-    object edum: TEdit
-      Left = 8
-      Top = 48
-      Width = 64
-      Height = 21
-      Alignment = taRightJustify
-      Color = clMenu
-      TabOrder = 2
-    end
-    object edmil: TEdit
-      Left = 8
-      Top = 115
-      Width = 64
-      Height = 21
-      Alignment = taRightJustify
-      Color = clMenu
-      TabOrder = 3
-    end
-    object btnCalc: TButton
-      Left = 80
-      Top = 16
-      Width = 24
-      Height = 24
-      Caption = 'c'
-      Default = True
-      TabOrder = 4
-      OnClick = btnCalcClick
-    end
   end
   object btnClose: TButton
-    Left = 16
-    Top = 176
-    Width = 112
-    Height = 40
+    Left = 46
+    Top = 378
+    Width = 80
+    Height = 24
     Cancel = True
     Caption = 'Close'
-    TabOrder = 1
+    TabOrder = 5
     OnClick = btnCloseClick
+  end
+  object GroupBox1: TGroupBox
+    Left = 16
+    Top = 260
+    Width = 152
+    Height = 88
+    Caption = 'Metric'
+    TabOrder = 4
+    object lblum: TLabel
+      Left = 120
+      Top = 61
+      Width = 14
+      Height = 13
+      Caption = #181'm'
+    end
+    object lblmm: TLabel
+      Left = 120
+      Top = 29
+      Width = 16
+      Height = 13
+      Caption = 'mm'
+    end
+    object edum: TEdit
+      Left = 16
+      Top = 56
+      Width = 88
+      Height = 21
+      Alignment = taRightJustify
+      Color = clMenu
+      TabOrder = 1
+    end
+    object edmm: TEdit
+      Left = 16
+      Top = 24
+      Width = 88
+      Height = 21
+      Alignment = taRightJustify
+      Color = clMenu
+      TabOrder = 0
+    end
+  end
+  object edInput: TEdit
+    Left = 42
+    Top = 60
+    Width = 96
+    Height = 21
+    Alignment = taRightJustify
+    ParentShowHint = False
+    ShowHint = False
+    TabOrder = 0
+  end
+  object btnCalc: TButton
+    Left = 8
+    Top = 108
+    Width = 169
+    Height = 40
+    Caption = 'Convert'
+    Default = True
+    TabOrder = 2
+    OnClick = btnCalcClick
+  end
+  object chbClipBrd: TCheckBox
+    Left = 8
+    Top = 85
+    Width = 170
+    Height = 17
+    Hint = 
+      'If you check this box, the result will be immediatly available w' +
+      'ith '#39'Copy > Paste'#39' or CTRL + V.'
+    HelpType = htKeyword
+    Caption = 'Copy the result to the clipboard'
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 1
   end
 end
--- trunk/mil-mm-conv/mil-mm-conv.pas
+++ trunk/mil-mm-conv/mil-mm-conv.pas
@@ -2,8 +2,8 @@
 { Easy to use calculator for converting units.  }
 {                                               }
 { Enter a numeric value including unit...       }
-{ [mm, mil, um] -> e.g. 3.1415 mm               }
-{ ...then press enter (or the small 'c' button).}
+{ [mm, in, mil, um] -> e.g. 3.1415 mm           }
+{ ...then press enter (or the 'Convert' button).}
 { Converted values will be shown in all units.  }
 { Spaces between number and unit are ignored.   }
 { '.' and ',' are accepted as decimal separator.}
@@ -11,24 +11,68 @@
 { makes it easy to calculate multiple values.   }
 {                                               }
 { Author: cyril@andreatta.ch                    }
-{ Version: 1.0                                  }
+{ Version: 2.0                                  }
+{ Improved by: Justin MASSIOT, m.just1@free.fr  }
+{-----------------------------------------------}
+{                                               }
+{ Changelog:                                    }
+{                                               }
+{ -----                                         }
+{ 2.0                                           }
+{ Improvements by Justin MASSIOT                }
+{ * New unit 'in' (inch) available              }
+{ * Copy to clipboard automatically             }
+{ * Parameters for user configuration           }
+{ * New GUI, InitCreate() and InitShow()        }
+{                                               }
+{ -----                                         }
+{ 1.0                                           }
+{ Original version by Cyril ANDREATTA           }
+{                                               }
 {-----------------------------------------------}
 
+//uses ClipBrd;
+
+const
+    floatformat = '0.####'; // configuration: define the format of the decimal results; default is '0.####' (round to the fourth decimal, no trailing zeros but with a leading zero before the separator), leave this empty if you do not ant to round the results
+    copybydefault = true; // configuration: choose to copy the result to clipboard by default at startup; default is 'true' = copy by default
+    copymetric = 'mm'; // configuration: choose the result to copy to clipboard after a conversion from imperial to metric; could be 'mm' or 'um', default is 'mm'
+    copyimperial = 'mil'; // configuration: choose the result to copy to clipboard after a conversion from metric to imperial; could be 'mil' or 'in', default is 'mil'
+    copywithunit = true; // configuration: choose to copy the raw value, or the value followed by the unit; default is 'false' = no unit
+    inputcolor = clBlue; // configuration: define the font color of the value which is the input
+    copycolor = clSkyBlue; // configuration: define the background color of the result which has been copied to the clipboard; default is clSkyBlue, other proposals are clActiveCaption, clHighlight
+
 var
-	lastunit:String; // global variable to store last unit
+    lastunit:String; // global variable to store last unit
 
 procedure TfrmCalc.btnCalcClick(Sender: TObject);
 var
-	i		:Integer;
-	value   :String;
-    numstr	:String;
+    i       :Integer;
+    value   :String;
+    numstr  :String;
     unitstr :String;
-    num		:Float;
+    num     :Float;
 
 begin
-	i:=1;
+    i:=1;
     numstr:='';
     unitstr:='';
+    
+    // reset the TEdit font colours
+    edum.Font.Color := clWindowText;
+    edmm.Font.Color := clWindowText;
+    edinch.Font.Color := clWindowText;
+    edmil.Font.Color := clWindowText;
+    // reset the TEdit box background colours
+    edum.Color := clMenu;
+    edmm.Color := clMenu;
+    edinch.Color := clMenu;
+    edmil.Color := clMenu;
+    // reset the clipboard help text visibility
+    lblClipBrd.Visible := false;
+    //chbClipBrd.Checked := true;
+    
+    // ---------- ---------- ---------- ----------
 
     // read value from textbox
     value := edInput.Text;
@@ -36,7 +80,7 @@ begin
 
     // extract numeric value
     while (((value[i] >= '0') and
-    	  (value[i] <= '9')) or
+          (value[i] <= '9')) or
           (value[i] = '.') or
           (value[i] = ',')) and
           (i <= length(value)) do
@@ -46,14 +90,14 @@ begin
     end;
     // check if it is a real number
     try
-    	num:=StrToFloat(numstr);
+        if ( (value[1] >= '0') and (value[1] <= '9') ) then num:=StrToFloat(numstr);
     except
         exit
     end;
 
     // ignore spaces between number and unit
     while (value[i] = ' ') do
-    	i:=i+1;
+        i:=i+1;
 
     // extract unit
     while (value[i] >= 'a') and (value[i] <= 'z') do
@@ -65,41 +109,148 @@ begin
     // if unit is missing try the same as for last calculation
     if unitstr = '' then
         unitstr:=lastunit;
+    
+    // ---------- ---------- ---------- ----------
 
     case unitstr of
-    'mm':
-    	begin
-        edum.Text:=FloatToStr(num * 1000);
-        edmm.Text:=FloatToStr(num);
-        edmil.Text:=FloatToStr(num / 0.0254);
+    'in':
+        begin
+        edinch.Font.Color := inputcolor; // set a colour to the input value
+        edum.Text:=FormatFloat(floatformat, num * 25400);
+        edmm.Text:=FormatFloat(floatformat, num * 25.4);
+        edinch.Text:=FormatFloat(floatformat, num);
+        edmil.Text:=FormatFloat(floatformat, num * 1000);
         end;
     'mil':
-    	begin
-        edum.Text:=FloatToStr(num * 25.4);
-        edmm.Text:=FloatToStr(num * 0.0254);
-        edmil.Text:=FloatToStr(num);
+        begin
+        edmil.Font.Color := inputcolor; // set a colour to the input value
+        edum.Text:=FormatFloat(floatformat, num * 25.4);
+        edmm.Text:=FormatFloat(floatformat, num * 0.0254);
+        edinch.Text:=FormatFloat(floatformat, num / 1000);
+        edmil.Text:=FormatFloat(floatformat, num);
+        end;
+    'mm':
+        begin
+        edmm.Font.Color := inputcolor; // set a colour to the input value
+        edum.Text:=FormatFloat(floatformat, num * 1000);
+        edmm.Text:=FormatFloat(floatformat, num);
+        edinch.Text:=FormatFloat(floatformat, num / 25.4);
+        edmil.Text:=FormatFloat(floatformat, num / 0.0254);
         end;
     'um':
-    	begin
-        edum.Text:=FloatToStr(num);
-        edmm.Text:=FloatToStr(num / 1000);
-        edmil.Text:=FloatToStr(num / 25.4);
+        begin
+        edum.Font.Color := inputcolor; // set a colour to the input value
+        edum.Text:=FormatFloat(floatformat, num);
+        edmm.Text:=FormatFloat(floatformat, num / 1000);
+        edinch.Text:=FormatFloat(floatformat, num / 25400);
+        edmil.Text:=FormatFloat(floatformat, num / 25.4);
         end;
     else
-    	// unknown value
-    	ShowMessage('I can''t understand what you sayin...' + #10 +
-        			'Enter a numeric value followed by either ''mil'', ''mm'' or ''um''' + #10 +
-                    'e.g:' + #10 +
-                    '12 mil');
+        // unknown value
+        ShowMessage('I can''t understand what you sayin...' + #10 +
+                    'Enter a numeric value followed by either ''in'', ''mil'', ''mm'' or ''um''' + #10 +
+                    'e.g: 12.3 mil');
     end;
+    
+    // ---------- ---------- ---------- ----------
+    
+    // copy the result to the clipboard (if the user wants to)
+    //ShowMessage(chbClipBrd.Checked);
+    if (chbClipBrd.Checked = true) then
+    begin
+        if (unitstr = 'mil') or (unitstr = 'in') then // imperial -> metric conversion
+            begin
+            if (copymetric = 'um') then // copy the 'um' result
+                begin
+                edum.Color := copycolor; // set a background colour to the value that will be copied to the clipboard
+                lblClipBrd.Visible := true; // set the clipboard help text visible
+                if (copywithunit = true) then
+                    Clipboard.AsText := edum.Text + 'µm'
+                else
+                    begin
+                    edum.SelectAll;
+                    edum.CopyToClipboard;
+                    end;
+                end
+            else // copy the 'mm' result by default
+                begin
+                edmm.Color := copycolor; // set a background colour to the value that will be copied to the clipboard
+                lblClipBrd.Visible := true; // set the clipboard help text visible
+                if (copywithunit = true) then
+                    Clipboard.AsText := edmm.Text + 'mm'
+                else
+                    begin
+                    edmm.SelectAll;
+                    edmm.CopyToClipboard;
+                    end;
+                end;
+            end
+        else if (unitstr = 'mm') or (unitstr = 'um') then // metric -> imperial conversion
+            begin
+            if (copyimperial = 'in') then // copy the 'in' result
+                begin
+                edinch.Color := copycolor; // set a background colour to the value that will be copied to the clipboard
+                lblClipBrd.Visible := true; // set the clipboard help text visible
+                if (copywithunit = true) then
+                    Clipboard.AsText := edinch.Text + 'inch'
+                else
+                    begin
+                    edinch.SelectAll;
+                    edinch.CopyToClipboard;
+                    end;
+                end
+            else // copy the 'mil' result by default
+                begin
+                edmil.Color := copycolor; // set a background colour to the value that will be copied to the clipboard
+                lblClipBrd.Visible := true; // set the clipboard help text visible
+                if (copywithunit = true) then
+                    Clipboard.AsText := edmil.Text + 'mil'
+                else
+                    begin
+                    edmil.SelectAll;
+                    edmil.CopyToClipboard;
+                    end;
+                end;
+            end;
+        // else: the specified unit is not within the valid list of units
+        
+        // now set the "clipboard label" to show the text contained in the clipboard
+        lblClipBrd.Caption := 'Clipboard = ' + Clipboard.AsText;
+    end;
+    // else: the user does not want to copy the result to the clipboard
+    
+    // ---------- ---------- ---------- ----------
 
     lastunit:=unitstr;
 
     // clear input text
-	edInput.Text:='';
+    edInput.Text:='';
+    // show the unit in the GUI
+    lblUnit.Caption:=lastunit;
 end;
 
 procedure TfrmCalc.btnCloseClick(Sender: TObject);
 begin
     Close;
 end;
+  
+procedure TfrmCalc.InitCreate(Sender: TObject); // OnCreate event triggered when the TForm is created (http://delphi.about.com/od/formsdialogs/a/delphiformlife.htm)
+                                                // see the *.dfm file for OnCreate -> InitCreate function call association
+begin
+    // set default states for the clipboard controls and info
+    lblClipBrd.Visible := false;
+    chbClipBrd.Checked := copybydefault;
+end;
+  
+procedure TfrmCalc.InitShow(Sender: TObject); // OnShow event triggered when the ShowModal procedure is called (http://delphi.about.com/od/formsdialogs/a/delphiformlife.htm)
+                                              // see the *.dfm file for OnShow -> InitShow function call association
+begin
+    //
+end;
+
+procedure ShowCalc;
+begin
+    frmCalc.ShowModal();
+end;
+
+
