86908896281aeb12465a721d007cc7c72d6043b7
BrettLMiller <54195045+BrettLMiller@users.noreply.github.com>
2022-08-09 10:08:41 +1200

    Update TentingVias.pas
    must set manual solder mask expansion before tenting can be set.

--- Scripts - PCB/TentingVias/TentingVias.pas
+++ Scripts - PCB/TentingVias/TentingVias.pas
@@ -1,12 +1,13 @@
-{-----------------------------------------------}
-{ Script with GUI to make all vias tenting or   }
-{ remove tenting from all vias.                 }
-{ It does also work on selected vias only.      }
-{                                               }
-{ Author: cyril@andreatta.ch                    }
-{ Version 1.1: Improved tenting function for    }
-{ when only some vias are selected.             }
-{-----------------------------------------------}
+{------------------------------------------------------------------------------}
+{ Script with GUI to make all vias tenting or                                  }
+{ remove tenting from all vias.                                                }
+{ It does also work on selected vias only.                                     }
+{                                                                              }
+{ Author: cyril@andreatta.ch                                                   }
+{ Version 1.1: Improved tenting function for when only some vias are selected.
+          1.2: BLM Fixed display update in any view & set soldermask
+                   expansion = manual to allow tenting to be set.
+-------------------------------------------------------------------------------}
 
 Var
     Board     : IPCB_Board;
@@ -14,12 +15,14 @@ Var
 {-- Function to count all vias already tented --}
 Procedure CountVias(rw : Boolean = false);
 Var
-    Via       : IPCB_Primitive;
-    Iterator  : IPCB_BoardIterator;
-    ViaCount  : Integer;
-    TopTenting: Integer;
-    BotTenting: Integer;
-    ViaSelected: Integer;
+    Via         : IPCB_Primitive;
+    PadCache    : TPadCache;
+    Iterator    : IPCB_BoardIterator;
+    ViaCount    : Integer;
+    TopTenting  : Integer;
+    BotTenting  : Integer;
+    ViaSelected : Integer;
+
 Begin
     ViaCount      := 0;
     TopTenting    := 0;
@@ -31,18 +34,23 @@ Begin
     Iterator.AddFilter_ObjectSet(MkSet(eViaObject));
     Iterator.AddFilter_LayerSet(AllLayers);
     Iterator.AddFilter_Method(eProcessAll);
-
     // Search and count vias
     Via := Iterator.FirstPCBObject;
     While (Via <> Nil) Do
     Begin
         If rw = true Then
         Begin
-            // make vias tenting or not depending on checkbox states
+            // make vias tented or not depending on checkbox states
             If ((chkSelected.Checked = false) or ((chkselected.Checked = true) and (Via.Selected))) Then
             Begin
+                Via.BeginModify;
+                PadCache := Via.Cache;
+                PadCache.SolderMaskExpansionValid := eCacheManual;
+                Via.SetState_Cache := PadCache;
+//                Via.Setstate_PadCacheRobotFlag(true);
                 Via.SetState_IsTenting_Top(chkTop.Checked);
                 Via.SetState_IsTenting_Bottom(chkBottom.Checked);
+                Via.EndModify;
             End;
         End
         Else
@@ -57,15 +65,19 @@ Begin
                Inc(BotTenting);
         End;
         Inc(ViaCount);
+
+        Board.ViewManager_GraphicallyInvalidatePrimitive(Via);
+
         Via := Iterator.NextPCBObject;
     End;
+
     Board.BoardIterator_Destroy(Iterator);
 
     // Display the count results on the form
-    lblViaCount.Caption := IntToStr(ViaCount);
+    lblViaCount.Caption   := IntToStr(ViaCount);
     lblTopTenting.Caption := IntToStr(TopTenting);
     lblBotTenting.Caption := IntToStr(BotTenting);
-    lblSelected.Caption := IntToStr(ViaSelected);
+    lblSelected.Caption   := IntToStr(ViaSelected);
 
     If ViaSelected = 0 Then
         chkSelected.Enabled := false;
@@ -75,6 +87,7 @@ Begin
     If BotTenting > ViaCount/2 Then chkBottom.Checked := true;
 
     // Refresh PCB screen
+    Board.ViewManager_FullUpdate;
     Client.SendMessage('PCB:Zoom', 'Action=Redraw' , 255, Client.CurrentView);
 End;
 
@@ -95,12 +108,10 @@ begin
     Board := PCBServer.GetCurrentPCBBoard;
     If Board = Nil Then
     Begin
-        ShowWarning('This document is not a PCB document!' + #13 +
-                    'Therefore Tenting script will not be executed.');
+        ShowWarning('Focused document is not a PCB !' + #13 +
+                    'Tenting script will not execute.');
         Exit;
-    End
-    Else
-        CountVias;
     End;
-end;
 
+    CountVias;
+end;
