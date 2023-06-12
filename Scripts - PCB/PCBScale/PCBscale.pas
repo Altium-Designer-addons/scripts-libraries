628178fb0830a5f690f067064ecba6d8951455d0
RnDMonkey <RnDMonkey@users.noreply.github.com>
2023-06-12 13:10:45 -0700

    Update PCBscale.pas
    * merged V2 from forum with v1.1
    * changed anchor selection and added more options
    * streamlined some operations to refocus on scale input

--- Scripts - PCB/PCBScale/PCBscale.pas
+++ Scripts - PCB/PCBScale/PCBscale.pas
@@ -8,10 +8,21 @@
 {                                                                              }
 {           Changelog:                                                         }
 {           - v1.0 - Initial Release                                           }
+{                                                                              }
 {           - v1.1 - updated for text objects in AD19+, some format cleanup    }
 {                                                                              }
+{           - v2.0 - some changes (wishes for forum)                           }
+{           - added Pre- and Post Process for Undo                             }
+{           - added edit fields for entering new Boardsize                     }
+{             (the scale factor will recalculated from this value              }
+{           - added combo box to select Ref Point (center was wanted)          }
+{                                                                              }
+{           - v2.1 - merged V2 from forum with v1.1                            }
+{           - changed anchor selection and added more options                  }
+{           - streamlined some operations to refocus on scale input            }
+{                                                                              }
 { Created by:    Petar Perisin                                                 }
-{ Updated by:    Ryan Rutledge                                                 }
+{ Updated by:    Ryan Rutledge (with motivation by Dennis Saputelli)           }
 {..............................................................................}
 
 {..............................................................................}
@@ -20,6 +31,28 @@ var
     Ratio           : Float;
     Board           : IPCB_Board;
     IsAtLeastAD19   : Boolean;
+
+const
+    ScriptVersion = '2.1';
+    ScriptTitle = 'PCBScale';
+
+
+procedure About;
+var
+    MsgText : string;
+begin
+    MsgText := '"' + ScriptTitle + '" script version ' + ScriptVersion + sLineBreak + sLineBreak +
+        'Updated versions may be found here:' + sLineBreak +
+        'https://github.com/Altium-Designer-addons/scripts-libraries';
+
+    ShowInfo(MsgText, 'About');
+end;
+
+
+procedure TFormPCBscale.LabelVersionClick(Sender: TObject);
+begin
+    About;
+end;
 
 
 function IsStringANum(Text : string) : Boolean;
@@ -42,6 +75,8 @@ begin
         if InSet(Ord(Text[i]), ChSet) then inc(dotCount);
 
     if dotCount > 1 then Result := False;
+
+    if Length(Text) = 0 then Result := False;
 end;
 
 
@@ -99,18 +134,50 @@ begin
 end;
 
 
-procedure TFormPCBScale.ButtonCancelClick(Sender: TObject);
+// V2 new helper function to update the values in the Editboxes and the Label
+procedure UpdateFields(Sender: TObject);
+var
+    BR: TCoordRect;
+    TempString : String;
+    Scale: float;
+begin
+    BR := Board.BoardOutline.BoundingRectangle;
+    TempString := EditRatio.Text;
+    if not IsStringANum(TempString) then exit;
+    if LastDelimiter(',.', TempString) <> 0 then TempString[LastDelimiter(',.', TempString)] := DecimalSeparator;
+    scale:= StrToFloat(TempString);
+    if RBmm.Checked then
+    begin
+        LabelActualBoardSize.Caption := 'Boardsize = '+FloatToStr(CoordToMMs(BR.right-BR.left))+ 'mm x ' + FloatToStr(CoordToMMs(BR.top-BR.bottom))+'mm';
+        ENewWidth.Text := CoordToMMs((BR.right-BR.left)*Scale);
+        ENewHeight.Text:= CoordToMMs((BR.top-BR.bottom)*Scale);
+    end
+    else
+    begin
+        LabelActualBoardSize.Caption := 'Boardsize = '+FloatToStr(CoordToMils(BR.right-BR.left))+ 'mil x ' + FloatToStr(CoordToMils(BR.top-BR.bottom))+'mil';
+        ENewWidth.Text := CoordToMils((BR.right-BR.left)*Scale);
+        ENewHeight.Text:= CoordToMils((BR.top-BR.bottom)*Scale);
+    end;
+
+    if FormPCBscale.Showing and ((Sender = RBmm) or (Sender = RBmil)) then EditRatio.SetFocus;
+end;
+
+
+
+procedure TFormPCBscale.ButtonCancelClick(Sender: TObject);
 begin
     close;
 end;
 
 
-procedure TFormPCBScale.EditRatioChange(Sender: TObject);
+
+procedure TFormPCBscale.EditRatioChange(Sender: TObject);
 begin
     if IsStringANum(EditRatio.Text) then
     begin
         EditRatio.Font.Color := clWindowText;
         ButtonOK.Enabled := True;
+        UpdateFields(Sender);
     end
     else
     begin
@@ -387,12 +454,13 @@ end;
 // Not done
 Procedure ScaleDimension(Prim : IPCB_Primitive);
 begin
-
+    exit;   // not done
 end;
 
 // Not done - bad placement and can not refresh
 Procedure ScaleCoordinate(Prim : IPCB_Coordinate);
 begin
+    exit;   // not done
 
     Prim.LineWidth  := Int(Prim.LineWidth * Ratio);
     Prim.Size       := Int(Prim.Size * Ratio);
@@ -478,8 +546,8 @@ begin
             eTextObject          : ScaleText(Prim);
             ePolyObject          : ScalePoly(Prim);
             eComponentBodyObject : Scale3DModel(Prim);
-        // eDimensionObject     : ScaleDimension(Prim);
-        // eCoordinateObject    : ScaleCoordinate(Prim);
+            eDimensionObject     : ScaleDimension(Prim);    // not done
+            eCoordinateObject    : ScaleCoordinate(Prim);   // not done
         end;
         Prim.EndModify;
         Prim.GraphicallyInvalidate;
@@ -493,33 +561,176 @@ begin
 end;
 
 
-procedure TFormPCBScale.ButtonOKClick(Sender: TObject);
+// V2 a new width for the boardsize was entered recalculate Scalefactor and update fields
+procedure TFormPCBscale.ENewWidthExit(Sender: TObject);
+var
+    TempString : String;
+    Scale: float;
+    BR: TCoordRect;
+    NewWidth: integer;
+begin
+    BR := Board.BoardOutline.BoundingRectangle;
+    TempString := ENewWidth.Text;
+    if LastDelimiter(',.', TempString) <> 0 then TempString[LastDelimiter(',.', TempString)] := DecimalSeparator;
+    if RBmm.Checked then
+        NewWidth:= mmstocoord(StrToFloat(TempString))
+    else
+        NewWidth:= milstocoord(StrToFloat(TempString));
+    Scale:=NewWidth/(BR.right-BR.left);
+    EditRatio.Text:=floattostrf(Scale,ffGeneral,5,0);
+    UpdateFields(Sender);
+end;
+
+// V2 a new Height for the boardsize was entered recalculate Scalefactor and update fields
+procedure TFormPCBscale.ENewHeightExit(Sender: TObject);
+var
+    TempString : String;
+    Scale: float;
+    BR: TCoordRect;
+    NewHeight: integer;
+begin
+    BR := Board.BoardOutline.BoundingRectangle;
+    TempString := ENewHeight.Text;
+    if LastDelimiter(',.', TempString) <> 0 then TempString[LastDelimiter(',.', TempString)] := DecimalSeparator;
+    if RBmm.Checked then
+        NewHeight:= mmstocoord(StrToFloat(TempString))
+    else
+        NewHeight:= milstocoord(StrToFloat(TempString));
+    Scale:=NewHeight/(BR.top-BR.bottom);
+    EditRatio.Text:=floattostrf(Scale,ffGeneral,5,0);
+    UpdateFields(Sender);
+end;
+
+
+procedure FocusEditRatio(Sender : TObject);
+begin
+    EditRatio.SetFocus;
+end;
+
+
+function GetAnchorChoice(GB : TGroupBox) : TRadioButton;
+var
+    i : Integer;
+    radioButton : TRadioButton;
+begin
+    Result := nil;
+
+    for i:= 0 to GB.ControlCount - 1 do
+    begin
+        radioButton := GB.Controls[i];
+        if radioButton.Checked then
+        begin
+            Result := radioButton;
+            break;
+        end;
+    end;
+end;
+
+
+procedure TFormPCBscale.GroupBoxAnchorClick(Sender: TObject);
+begin
+    //ShowInfo(GetAnchorChoice(GroupBoxAnchor).Name);   // for debugging
+end;
+
+
+procedure ScaleSelection(const dummy : Boolean);
 var
     Rectang    : TCoordRect;
     TempString : String;
     i          : Integer;
     Prim       : IPCB_Primitive;
+    left,right,top,bottom  : Integer;
 begin
     Board := PCBServer.GetCurrentPCBBoard;
 
     if Board = nil then exit;
     if Board.SelectecObjectCount = 0 then exit;
 
+    // V2 added PreProcess for Undo
+    PCBServer.PreProcess;       // Initialize robots in PCB Server (for UNDO)
+
     TempString := EditRatio.Text;
     if LastDelimiter(',.', TempString) <> 0 then TempString[LastDelimiter(',.', TempString)] := DecimalSeparator;
 
     Ratio := StrToFloat(TempString);
 
-    X := Board.SelectecObject[0].BoundingRectangle.Left;
-    Y := Board.SelectecObject[0].BoundingRectangle.Bottom;
+    if not (Ratio > 0) then
+    begin
+        ShowError('Scale cannot be 0');
+        exit;
+    end;
 
+    // V2 changed/added find not only left and bottom minimum also right and top maximum (for Ref Point)
+    left  := Board.SelectecObject[0].BoundingRectangle.Left;
+    right := Board.SelectecObject[0].BoundingRectangle.Right;
+    top   := Board.SelectecObject[0].BoundingRectangle.Top;
+    bottom:= Board.SelectecObject[0].BoundingRectangle.Bottom;
+
+    // original
+    //X := Board.SelectecObject[0].BoundingRectangle.Left;
+    //Y := Board.SelectecObject[0].BoundingRectangle.Bottom;
+    // get min max for X,Y Coordinates (go thru all selected objects and find most left,right.. coordinate)
     for i := 0 to Board.SelectecObjectCount - 1 do
     begin
         Prim := Board.SelectecObject[i];
-        if X > Prim.BoundingRectangle.Left then
-            X := Prim.BoundingRectangle.Left;
-        if Y > Prim.BoundingRectangle.Bottom then
-            Y := Prim.BoundingRectangle.Bottom;
+        if left > Prim.BoundingRectangle.Left then
+            left := Prim.BoundingRectangle.Left;
+        if right < Prim.BoundingRectangle.Right then
+            right := Prim.BoundingRectangle.Right;
+        if top < Prim.BoundingRectangle.top then
+            top := Prim.BoundingRectangle.top;
+        if bottom > Prim.BoundingRectangle.Bottom then
+            bottom := Prim.BoundingRectangle.Bottom;
+    end;
+
+    // V2 added set Ref Point (variable X,Y) according combobox selection
+    // RBTopLeft, RBCenterLeft, RBBottomLeft, RBTopCenter, RBCenter, RBBottomCenter, RBTopRight, RBCenterRight, RBBottomRight;
+    case GetAnchorChoice(GroupBoxAnchor).Tag of
+        1:  // RBTopLeft :
+        begin
+            X := left;
+            Y := top;
+        end;
+        2:  // RBCenterLeft :
+        begin
+            X := left;
+            Y := bottom+(top-bottom)/2;
+        end;
+        3:  // RBBottomLeft :
+        begin
+            X := left;
+            Y := bottom;
+        end;
+        4:  // RBTopCenter :
+        begin
+            X := left+(right-left)/2;
+            Y := top;
+        end;
+        5:  // RBCenter :
+        begin
+            X := left+(right-left)/2;
+            Y := bottom+(top-bottom)/2;
+        end;
+        6:  // RBBottomCenter :
+        begin
+            X := left+(right-left)/2;
+            Y := bottom;
+        end;
+        7:  // RBTopRight :
+        begin
+            X := right;
+            Y := top;
+        end;
+        8:  // RBCenterRight :
+        begin
+            X := right;
+            Y := bottom+(top-bottom)/2;
+        end;
+        9:  // RBBottomRight :
+        begin
+            X := right;
+            Y := bottom;
+        end;
     end;
 
     for i := 0 to Board.SelectecObjectCount - 1 do
@@ -548,8 +759,28 @@ begin
             Prim.GraphicallyInvalidate;
         end;
     end;
+    // V2 added PostProcess for Undo
+    Pcbserver.PostProcess;  // Clean up robots in PCB Server    (for UNDO)
+
     Board.ViewManager_FullUpdate;
     close;
+end;
+
+
+procedure TFormPCBscale.ButtonOKClick(Sender: TObject);
+begin
+    ScaleSelection(True);
+end;
+
+
+// programmatically, OnKeyPress fires before OnChange event and "catches" the key press
+procedure UserKeyPress(Sender : TObject; var Key : Char);
+begin
+    if (ButtonOK.Enabled) and (Ord(Key) = 13) then
+    begin
+        Key := #0; // catch and discard key press to avoid beep
+        if Sender = EditRatio then ScaleSelection(False) else EditRatio.SetFocus;
+    end;
 end;
 
 
@@ -563,8 +794,15 @@ begin
         ShowMessage('There are no selected objects');
         exit;
     end;
+    // V2 update Editboxes and Label before showing the form
+    UpdateFields(nil);
 
+    // set version label
+    LabelVersion.Caption := '"' + ScriptTitle + '" v' + ScriptVersion;
+
+    // set AD build flag
     if (GetBuildNumberPart(Client.GetProductVersion, 0) >= 19) then IsAtLeastAD19 := True else IsAtLeastAD19 := False;
-    FormPCBScale.ShowModal;
+
+    FormPCBscale.ShowModal;
 end;
 
