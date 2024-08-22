388aa5b6a28cb9a77d3407613d60b5ef670dea7b
Cyrrus <rechichi.daniel@gmail.com>
2024-08-22 13:39:22 +0800

    Added prefix, suffix and preview capabilities.

--- Scripts - PCB/RenumberPads/RenumberPads.pas
+++ Scripts - PCB/RenumberPads/RenumberPads.pas
@@ -20,12 +20,24 @@ procedure TRenumberPads.btnOKClick(Sender: TObject);
 Var
     PadIndex     : Integer;
     PadIncrement : Integer;
+    PadPrefix    : String;
+    PadSuffix    : String;
+    PadDesignator : String;
 
 Begin
 
-     // Get requested first index number
+     // Get requested first index number.
     PadIndex := StrToInt(edFirstPadNumber.Text);
     PadIncrement := StrToInt(edPadIncrement.Text);
+
+    // Get requested prefix and suffix, if enabled.
+    if cbPrefix.Checked then PadPrefix := edPrefix.Text
+    else PadPrefix := '';
+
+    if cbSuffix.Checked then PadSuffix := edSuffix.Text
+    else PadSuffix := '';
+
+    // Hide window.
     RenumberPads.Visible := 0;
 
      // Ask user to select first pad object
@@ -38,13 +50,15 @@ Begin
         PCBServer.SendMessageToRobots(PadObject.I_ObjectAddress, c_Broadcast,
                                PCBM_BeginModify , c_NoEventData);
          // change pad index
-        PadObject.Name := PadIndex;
+        PadObject.Name := format('%S%S%S',[PadPrefix, IntToStr(PadIndex), PadSuffix]);
         PCBServer.SendMessageToRobots(PadObject.I_ObjectAddress, c_Broadcast,
                                PCBM_EndModify , c_NoEventData);
         PCBServer.PostProcess;
 
+        // Increment the current pad index for the next loop.
         PadIndex := PadIndex + PadIncrement;
-         // ask user to select next pad in infinite loop
+
+        // Ask user to select next pad in infinite loop.
         PadObject := Board.GetObjectAtCursor(MkSet(ePadObject), AllLayers,
                                       'Choose a pad');
     End;
@@ -53,7 +67,7 @@ Begin
 End;
 
 
-
+// Cancel button event.
 procedure TRenumberPads.btnCancelClick(Sender: TObject);
 begin
      Close;
@@ -77,3 +91,46 @@ begin
 
 end;
 
+
+// Enable or disable the designator prefix option.
+procedure TRenumberPads.cbPrefixClick(Sender: TObject);
+begin
+     if cbPrefix.Checked then edPrefix.Enabled := True
+     else edPrefix.Enabled := False;
+end;
+
+
+// Enable or disable the designator suffix option.
+procedure TRenumberPads.cbSuffixClick(Sender: TObject);
+begin
+     if cbSuffix.Checked then edSuffix.Enabled := True
+     else edSuffix.Enabled := False;
+end;
+
+
+// Generate a preview of the designator format.
+procedure TRenumberPads.btnPreviewClick(Sender: TObject);
+Var
+     PadIndex     : Integer;
+     PadIncrement : Integer;
+     PadPrefix    : String;
+     PadSuffix    : String;
+     PadDesignator : String;
+     PadDesignatorNext      : String;
+
+begin
+     PadIndex := StrToInt(edFirstPadNumber.Text);
+     PadIncrement := StrToInt(edPadIncrement.Text);
+
+     if cbPrefix.Checked then PadPrefix := edPrefix.Text
+     else PadPrefix := '';
+
+     if cbSuffix.Checked then PadSuffix := edSuffix.Text
+     else PadSuffix := '';
+
+     lblPreview.Visible := True;
+
+     PadDesignator := format('%S%S%S',[PadPrefix, IntToStr(PadIndex), PadSuffix]);
+     PadDesignatorNext := format('%S%S%S',[PadPrefix, IntToStr(PadIndex + PadIncrement), PadSuffix]);
+     lblPreview.Caption := format('%S, %S', [PadDesignator, PadDesignatorNext]);
+end;
